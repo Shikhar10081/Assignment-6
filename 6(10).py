31e5933e505625c1d87b124bc1b3b8802ba5102e
Shikhar10081 <111962558+Shikhar10081@users.noreply.github.com>
2022-09-02 11:52:08 +0530

    10,11,12

---
+++ 6(10).py
@@ -0,0 +1,11 @@
+print("Enter 3 number ")
+x,y,z=int(input()),int(input()),int(input())
+if x>y and x>z:
+     print("x")
+elif y>z:
+     print("y")
+else:
+    print("z")
+if x==y==z:
+    print(x)
+    
---
+++ 6(11).py
@@ -0,0 +1,9 @@
+x=int(input("Enter any month value:"))
+if x==1 or x==3 or x==5 or x==7 or x==8 or x==10 or x==12:
+     print("31 days")
+else :
+    if x==2:
+         print("28 days")
+    else:      
+        print("30 days")         
+        
